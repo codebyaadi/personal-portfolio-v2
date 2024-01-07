17e4cf56b62dd6079e59fc419227dfd422446a70
codebyaadi <adityastar57@gmail.com>
2024-01-07 17:59:02 +0530

    Added site info

---
+++ src/config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+    name: "Aditya Rajbhar",
+    creator: "codebyaadi",
+    title: "Aditya - Personal Portfolio",
+    description:
+        "Explore Aditya's skills, experience, and projects in web development and design. A Next.js powered personal portfolio.",
+    keywords: [
+        "Aditya",
+        "codebyaadi",
+        "portfolio",
+        "web developer",
+        "web designer",
+        "next.js portfolio",
+    ],
+    url: "https://codebyaadi.netlify.app/",
+};
