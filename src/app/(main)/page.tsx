07786b8203b62db828cd8554d41aaec79360423c
codebyaadi <adityastar57@gmail.com>
2024-01-12 13:54:48 +0530

    Removed build error for keys and added apos

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         transition={{ duration: 0.5 }}
         className="text-sm md:text-base tracking-tight mb-3"
       >
-        Hello! I'm <span className="font-medium">Aditya</span>, a passionate web designer and developer with a two-year journey in crafting digital experiences. I always had an interest in technology and the internet. Creating an innovative product is my specialty. I specialize in the MERN stack—MongoDB, Express.js, React, and Node.js—enabling me to create robust and dynamic web applications that seamlessly blend functionality and aesthetics.
+        Hello! I&apos;m <span className="font-medium">Aditya</span>, a passionate web designer and developer with a two-year journey in crafting digital experiences. I always had an interest in technology and the internet. Creating an innovative product is my specialty. I specialize in the MERN stack—MongoDB, Express.js, React, and Node.js—enabling me to create robust and dynamic web applications that seamlessly blend functionality and aesthetics.
       </motion.p>
       <motion.p
         initial={{ opacity: 0, y: 20 }}
@@ -22,7 +22,7 @@ export default function Home() {
         transition={{ duration: 0.5, delay: 0.25 }}
         className="text-sm md:text-base tracking-tight mb-3"
       >
-        Feel free to explore my portfolio and reach out if you have any questions or exciting opportunities to discuss. Let's build something amazing together!
+        Feel free to explore my portfolio and reach out if you have any questions or exciting opportunities to discuss. Let&apos;s build something amazing together!
       </motion.p>
       <motion.div
         initial={{ opacity: 0 }}
@@ -34,8 +34,8 @@ export default function Home() {
         }}
         className="flex flex-wrap gap-1 font-unbounded text-[10px] pt-1"
       >
-        {socialLinks.map((link) => (
-          <div className="px-3 py-2 rounded-sm bg-charcoalblue">
+        {socialLinks.map((link, index) => (
+          <div key={index} className="px-3 py-2 rounded-sm bg-charcoalblue">
             <Link className="hover:bg-linear-gradient hover:bg-clip-text hover:text-transparent" id={link.id.toString()} href={link.url} target="_blank">
               {link.name}
             </Link>
